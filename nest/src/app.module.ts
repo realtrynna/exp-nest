e06119b3c011223c328e310af6d6cf042850d7ec
realtrynna <realtrynna@gmail.com>
2022-12-13 22:34:53 +0900

    Feat: TyeORM 설치와 config 설정 추가

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -7,14 +7,27 @@ import { UserModule } from "./user/user.module";
 import { WorkspaceModule } from "./workspace/workspace.module";
 import { ChannelModule } from "./channel/channel.module";
 import { DmModule } from "./dm/dm.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
+        ConfigModule.forRoot({ isGlobal: true }), // isGlobal 전역 process.env
         UserModule,
         WorkspaceModule,
         ChannelModule,
         DmModule,
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            host: "localhost",
+            port: 3306,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASES,
+            entities: ["entities/*.js"],
+            synchronize: true, // init
+            logging: true, // raw query
+            charset: "utf8mb4",
+        }),
     ],
     controllers: [AppController],
     providers: [
