8ab12a5e93e8b1b57d1c4538b4ee2ed56a77057e
realtrynna <realtrynna@gmail.com>
2025-02-13 22:56:19 +0900

    busboy와 aws-sdk

--- event-emitter/src/uploads/upload.controller.ts
+++ event-emitter/src/uploads/upload.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Post, Req, Res } from "@nestjs/common";
 import * as Busboy from "busboy";
+import { S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
+import {
+    ProgressBar,
+    logger,
+} from "@aws-doc-sdk-examples/lib/utils/util-log.js";
 
 @Controller()
 export class UploadController {
@@ -8,18 +14,43 @@ export class UploadController {
         const bb = Busboy({ headers: req.headers });
 
         bb.on("file", (name, file, info) => {
-            console.log(info);
+            file.on("data", async (data) => {
+                try {
+                    const upload = new Upload({
+                        client: this.s3(),
+                        params: {
+                            Bucket: "",
+                            Key: "big",
+                            Body: data,
+                        }
+                    });
 
-            file.on("data", (data) => {
-                console.log(data);
+                    upload.on("httpUploadProgress", ({ loaded, total }) => {
+                        console.log(loaded, total)
+                    });
+
+                    const result = await upload.done();
+                    console.log(result);
+                } catch (err) {
+                    console.log(err);
+                }
             })
         });
 
         bb.on("close", () => {
-            console.log("close");
             res.end();
         });
 
         req.pipe(bb);
+    }
+
+    s3() {
+        return new S3Client({
+            region: "",
+            credentials: {
+                accessKeyId: "",
+                secretAccessKey: ""
+            }
+        })
     }
 }
